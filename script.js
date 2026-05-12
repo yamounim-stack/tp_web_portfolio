7f82f3483ce052c3d9fcfe17e97c11876ac7c1bb
MOUNIM Yassmine <ya.mounim@esisa.ac.ma>
2026-05-12 11:45:06 +0000

    Organisation de la page avec des couleurs6

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log(" lescript js est chargé");
+const button = document.getElementById("myButton");
+const message = document.getElementById("message");
+button.addEventListener("click", function() {
+    message.textContent = "Vous avez cliqué sur le bouton!";
+});
